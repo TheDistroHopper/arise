b1d80f6debc4631c665382d3fee8498dc7c8c0b1
Sarvesh Patil <sarvesh@sarve.sh>
2026-05-28 20:15:48 +0530

    feat: add plain chat with Language Model (LM) (#2)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import httpx
+from schemas import ChatMessage, ModelEnum
 
 
 app = FastAPI()
@@ -7,3 +9,26 @@ app = FastAPI()
 @app.get("/")
 def greet():
     return {"message": "Arise"}
+
+
+@app.get("/models")
+def list_language_models():
+    url = "http://localhost:11434/api/tags"
+    resp = httpx.get(url)
+    models = [model["name"] for model in resp.json()["models"]]
+    return models
+
+
+@app.post("/chat")
+def chat_with_language_model(model: ModelEnum, chat_message: ChatMessage):
+    payload = {
+        "model": model.value,
+        "messages": [{"role": "user", "content": chat_message.message}],
+        "stream": False,
+    }
+
+    url = "http://localhost:11434/api/chat"
+
+    resp = httpx.post(url, json=payload)
+
+    return {"message": resp.json()["message"]["content"]}
--- ui.py
+++ ui.py
@@ -2,9 +2,21 @@ import streamlit as st
 import httpx
 
 
-def get_greeting():
+def get_models():
     try:
-        resp = httpx.get("http://localhost:8000/")
+        resp = httpx.get("http://localhost:8000/models")
+        if resp.status_code == 200:
+            return resp.json()
+    except Exception as e:
+        print(f"ERROR: {e}")
+    return []
+
+
+def get_model_response(prompt: str, model: str):
+    try:
+        params = {"model": model}
+        payload = {"message": prompt}
+        resp = httpx.post("http://localhost:8000/chat", params=params, json=payload)
     except Exception as e:
         resp = None
         print(f"ERROR: {e}")
@@ -13,11 +25,27 @@ def get_greeting():
     return "ERROR"
 
 
-st.title("Arise")
-msg = get_greeting()
-if not msg == "ERROR":
-    st.badge("Let's warm up", icon=":material/check:", color="green")
-else:
-    st.badge(
-        "It seems you are down. Get some rest.", icon=":material/sleep:", color="red"
-    )
+with st.sidebar:
+    st.title(":material/robot: Arise")
+    models = get_models()
+    selected_model = st.selectbox("Select Model", models, label_visibility="collapsed")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+prompt = st.chat_input("What is up?")
+if prompt:
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = get_model_response(prompt, selected_model)
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
